df2be27f4b5143b4bc5c79aa4959b1f5f054df36
Dominique <ddordain@student.42.fr>
2022-08-02 17:31:54 +0200

    update

--- tmp_raycasting/ray_no_texture.c
+++ tmp_raycasting/ray_no_texture.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   raycasting3.c                                      :+:      :+:    :+:   */
+/*   ray_no_texture.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddordain <ddordain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/28 14:35:17 by ddordain          #+#    #+#             */
-/*   Updated: 2022/08/02 17:03:53 by ddordain         ###   ########.fr       */
+/*   Created: 2022/08/02 17:08:46 by ddordain          #+#    #+#             */
+/*   Updated: 2022/08/02 17:10:05 by ddordain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,43 +33,37 @@
 #define UP 65362
 #define DOWN 65364
 
-#define screenWidth 800
-#define screenHeigth 800
-#define texWidth 64
-#define texHeight 64
 #define mapWidth 24
 #define mapHeight 24
 #define width 800
 #define height 800
 
-int	worldMap[mapWidth][mapHeight] =
-						{
-							{4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,7,7,7,7,7,7,7,7},
-							{4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
-							{4,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
-							{4,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
-							{4,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
-							{4,0,4,0,0,0,0,5,5,5,5,5,5,5,5,5,7,7,0,7,7,7,7,7},
-							{4,0,5,0,0,0,0,5,0,5,0,5,0,5,0,5,7,0,0,0,7,7,7,1},
-							{4,0,6,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
-							{4,0,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,7,7,1},
-							{4,0,8,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
-							{4,0,0,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,7,7,7,1},
-							{4,0,0,0,0,0,0,5,5,5,5,0,5,5,5,5,7,7,7,7,7,7,7,1},
-							{6,6,6,6,6,6,6,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
-							{8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
-							{6,6,6,6,6,6,0,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
-							{4,4,4,4,4,4,0,4,4,4,6,0,6,2,2,2,2,2,2,2,3,3,3,3},
-							{4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
-							{4,0,0,0,0,0,0,0,0,0,0,0,6,2,0,0,5,0,0,2,0,0,0,2},
-							{4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
-							{4,0,6,0,6,0,0,0,0,4,6,0,0,0,0,0,5,0,0,0,0,0,0,2},
-							{4,0,0,5,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
-							{4,0,6,0,6,0,0,0,0,4,6,0,6,2,0,0,5,0,0,2,0,0,0,2},
-							{4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
-							{4,4,4,4,4,4,4,4,4,4,1,1,1,2,2,2,2,2,2,3,3,3,3,3}
+int	worldMap[24][24] = {
+							{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+							{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+							{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+							{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 						};
-
 
 typedef struct	s_info
 {
@@ -91,9 +85,7 @@ typedef struct	s_info
 	char	*addr;
 	int		bpp;
 	int		size_l;
-	int		endian;
-	int		texture[8][texHeight * texWidth];
-	int		**buff;
+	int		endian;	
 }				t_info;
 
 typedef struct	s_dda
@@ -117,16 +109,6 @@ typedef struct	s_dda
 	double	drawEnd;
 	int		color;
 }	t_dda;
-
-typedef struct	s_texture
-{
-	int		texNum;
-	double	wallX;
-	int		texX;
-	int		texY;
-	double	step;
-	double	texPos;
-}	t_tex;
 
 t_info	*_mlx_data(void)
 {
@@ -152,19 +134,6 @@ t_dda	*_dda(void)
 		first = FALSE;
 	}
 	return (&dda);
-}
-
-t_tex	*_tex(void)
-{
-	static t_bool	first = TRUE;
-	static t_tex	tex;
-
-	if (first == TRUE)
-	{
-		ft_bzero(&tex, sizeof(tex));
-		first = FALSE;
-	}
-	return (&tex);
 }
 
 void	img_pxl_put(int x, int y, int color)
@@ -206,38 +175,17 @@ void	init_mlx(void)
 	mlx_data->rotSpeed = 0.05;
 }
 
-void	draw(void)
+void	verLine(int x, int y1, int y2, int color)
 {
-	t_info	*info;
-
-	info = _mlx_data();
 	int	y;
-	int x;
 
-	y = 0;
-	while (y < height)
+	y = y1;
+	while (y <= y2)
 	{
-		x = 0;
-		while (x < width)
-		{
-			info->addr[y * width * x] = info->buff[y][x];
-		}
+		img_pxl_put(x, y, color);
 		y++;
 	}
-	mlx_put_image_to_window(info->mlx, info->mlx_win, info->img, 0, 0);
 }
-
-// void	verLine(int x, int y1, int y2, int color)
-// {
-	// int	y;
-// 
-	// y = y1;
-	// while (y <= y2)
-	// {
-		// img_pxl_put(x, y, color);
-		// y++;
-	// }
-// }
 
 void	init_dda_parameter(t_info info, int x)
 {
@@ -323,119 +271,25 @@ void	compute_wall_distance(t_info info)
 		dda->drawEnd = height - 1;
 }
 
-
-void	compute_texture(t_info info)
+void color_wall(void)
 {
-	t_dda	*d;
-	t_tex	*t;
+	t_dda	*dda;
 
-	d = _dda();
-	t = _tex();
-	t->texNum = worldMap[d->mapX][d->mapY];
-	if (d->side == 0)
-		t->wallX = info.posY + d->perpWallDist * d->rayDirY;
+	dda = _dda();
+	if (worldMap[dda->mapY][dda->mapX] == 1)
+		dda->color = 0xFF0000;
+	else if (worldMap[dda->mapY][dda->mapX] == 2)
+		dda->color = 0x00FF00;
+	else if (worldMap[dda->mapY][dda->mapX] == 3)
+		dda->color = 0x0000FF;
+	else if (worldMap[dda->mapY][dda->mapX] == 4)
+		dda->color = 0xFFFFFF;
 	else
-		t->wallX = info.posX + d->perpWallDist * d->rayDirX;
-	t->wallX -= floor(t->wallX);
-	t->texX = (int)(t->wallX * (double)texWidth);
-	if (d->side == 0 && d->rayDirX > 0)
-		t->texX = texWidth - t->texX - 1;
-	if (d->side == 1 && d->rayDirY < 0)
-		t->texX = texWidth - t->texX - 1;
-	t->step = 1.0 * texHeight / d->lineHeight;
-	t->texPos = (d->drawStart - height / 2 + d->lineHeight / 2) * t->step;
-}
-
-void	cast_texture(t_info info, int x)
-{
-	t_dda	*d;
-	t_tex	*t;
-	int		y1;
-	int		y2;
-
-	d = _dda();
-	t = _tex();
-	y1 = (int)d->drawStart;
-	y2 = (int)d->drawEnd;
-	printf("%d\n", y1);
-	while (y1 < y2)
-	{
-		t->texY = (int)t->texPos & (texHeight - 1);
-		t->texPos += t->step;
-		d->color = info.texture[t->texNum][texHeight * t->texY + t->texX];
-		if (d->side == 1)
-			d->color = (d->color >> 1) & 8355711;
-		printf("y = %d, x = %d\n", y, x);
-		info.buff[y1][x] = d->color;
-		y1++;
-	}
-}
-
-void	set_texture(void)
-{
-	t_info	*info;
-
-	info = _mlx_data();
-	info->buff = (int **)malloc(sizeof(int *) * height);
-	for (int i = 0; i < height; i++)
-	{
-		info->buff[i] = (int *)malloc(sizeof(int) * width);
-	}
-
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			info->buff[i][j] = 0;
-		}
-	}
-
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < texHeight * texWidth; j++)
-		{
-			info->texture[i][j] = 0;
-		}
-	}
-
-	for (int x = 0; x < texWidth; x++)
-	{
-		for (int y = 0; y < texHeight; y++)
-		{
-			int xorcolor = (x * 256 / texWidth) ^ (y * 256 / texHeight);
-			int ycolor = y * 256 / texHeight;
-			int xycolor = y * 128 / texHeight + x * 128 / texWidth;
-			info->texture[0][texWidth * y + x] = 65536 * 254 * (x != y && x != texWidth - y); //flat red texture with black cross
-			info->texture[1][texWidth * y + x] = xycolor + 256 * xycolor + 65536 * xycolor; //sloped greyscale
-			info->texture[2][texWidth * y + x] = 256 * xycolor + 65536 * xycolor; //sloped yellow gradient
-			info->texture[3][texWidth * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; //xor greyscale
-			info->texture[4][texWidth * y + x] = 256 * xorcolor; //xor green
-			info->texture[5][texWidth * y + x] = 65536 * 192 * (x % 16 && y % 16); //red bricks
-			info->texture[6][texWidth * y + x] = 65536 * ycolor; //red gradient
-			info->texture[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
-		}
-	}	
-}
-
-// void color_wall(void)
-// {
-// 	t_dda	*dda;
-
-// 	dda = _dda();
-// 	if (worldMap[dda->mapY][dda->mapX] == 1)
-// 		dda->color = 0xFF0000;
-// 	else if (worldMap[dda->mapY][dda->mapX] == 2)
-// 		dda->color = 0x00FF00;
-// 	else if (worldMap[dda->mapY][dda->mapX] == 3)
-// 		dda->color = 0x0000FF;
-// 	else if (worldMap[dda->mapY][dda->mapX] == 4)
-// 		dda->color = 0xFFFFFF;
-// 	else
-// 		dda->color = 0xFFFF00;
+		dda->color = 0xFFFF00;
 	
-// 	if (dda->side == 1)
-// 		dda->color = dda->color / 2;
-// }
+	if (dda->side == 1)
+		dda->color = dda->color / 2;
+}
 
 void	compute_raycasting(void)
 {
@@ -452,10 +306,8 @@ void	compute_raycasting(void)
 		compute_sidedistance(*info);
 		check_hit(*info);
 		compute_wall_distance(*info);
-		// color_wall();
-		// verLine(x, d->drawStart, d->drawEnd, d->color);
-		compute_texture(*info);
-		cast_texture(*info, x);
+		color_wall();
+		verLine(x, d->drawStart, d->drawEnd, d->color);
 		x++;
 	}
 }
@@ -512,8 +364,6 @@ void	button_left(void)
 	info->planeY = oldPlaneX * sin(info->rotSpeed) + info->planeY * cos(info->rotSpeed);	
 }
 
-
-// buttons feature
 int	buttons(int key)
 {
 	if (key == UP)
@@ -530,7 +380,6 @@ int	buttons(int key)
 	return (0);
 }
 
-// temp : display of the background
 void	display_background(void)
 {
 	t_info *mlx_data;
@@ -550,19 +399,17 @@ void	display_background(void)
 	}
 }
 
-// the display function.
 void	display(void)
 {
 	t_info *info;
 
 	info = _mlx_data();
-	// display_background();
+	display_background();
 	compute_raycasting();
 	mlx_put_image_to_window(info->mlx, info->mlx_win, info->img, 0, 0);
 }
 
-// looping the display with mlx parameter
-void	loop(void)
+int	main(void)
 {
 	t_info	*mlx_data;
 
@@ -572,10 +419,4 @@ void	loop(void)
 	mlx_hook(mlx_data->mlx_win, 2, 1L << 0, buttons, &mlx_data);
 	mlx_put_image_to_window(mlx_data->mlx, mlx_data->mlx_win, mlx_data->img, 0, 0);
 	mlx_loop(mlx_data->mlx);
-}
-
-int	main(void)
-{
-	set_texture();
-	loop();
 }
