b3b11709790c383daaf7460e0e861f0ac60375cc
Rakjlou <rakjlou@gmail.com>
2022-08-12 16:13:46 -0400

    Updated sky and ceiling colors, fixed potential segv

--- src/map/tile_collides.c
+++ src/map/tile_collides.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:50:19 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/08/11 23:01:56 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/08/12 16:10:27 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ t_bool	tile_collides_by_pos(int line, int column)
 	t_tile	*tile;
 
 	tile = map_get_tile(line, column);
+	if (tile == NULL)
+		return (TRUE);
 	return (tile_is_wall(tile));
 }
