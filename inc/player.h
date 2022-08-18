d666d5dde64644dfb1ca459f43b2609839244c48
Noe SIERRA VELASQUEZ <nsierra-@student.42.fr>
2022-08-18 17:18:17 +0200

    Adjusted player speed for school

--- inc/player.h
+++ inc/player.h
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 21:26:59 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/08/15 22:23:15 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/08/18 17:14:48 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # define PLAYER_START_POS_OFFSET 0.51
 # define PLAYER_DEFAULT_FOV 0.66
-# define PLAYER_DEFAULT_SPEED 0.05
+# define PLAYER_DEFAULT_SPEED 0.1
 # define PLAYER_DEFAULT_ROTATION_SPEED 0.05
 # define PLAYER_DEFAULT_MINIMAP_SIGHT 6
 
